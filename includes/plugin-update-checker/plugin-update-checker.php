dc9761773890865b6c35d658db581100983d593f
charltondigital <mail@aaroncharlton.com>
2026-07-16 11:23:32 -0700

    Update plugin-update-checker.php

--- includes/plugin-update-checker/plugin-update-checker.php
+++ includes/plugin-update-checker/plugin-update-checker.php
@@ -1,1 +1,13 @@
+<?php
+/**
+ * Plugin Update Checker Library Loader
+ * Version: 5.4
+ */
 
+if ( !class_exists('YahnisElsts\\PluginUpdateChecker\\v5\\PucFactory', false) ) {
+    require_once __DIR__ . '/Puc/v5/PucFactory.php';
+    require_once __DIR__ . '/Puc/v5/Autoloader.php';
+    
+    // Register the autoloader for the library classes
+    new YahnisElsts\PluginUpdateChecker\v5\Autoloader(__DIR__ . '/Puc/v5');
+}
